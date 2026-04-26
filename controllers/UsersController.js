fae924905a595f73d610461e7ed4b23ebadb8809
totomus83 <tom.dibellonio@gmail.com>
2026-04-26 18:53:21 +0200

    Task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,30 +3,25 @@ import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
-    const { email, password } = req.body;
+    const { email, password } = req.body || {};
 
-    // Missing email
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
     }
 
-    // Missing password
     if (!password) {
       return res.status(400).json({ error: 'Missing password' });
     }
 
     const db = dbClient.db;
 
-    // Check if user exists
     const userExists = await db.collection('users').findOne({ email });
     if (userExists) {
       return res.status(400).json({ error: 'Already exist' });
     }
 
-    // Hash password
     const hashedPassword = sha1(password);
 
-    // Insert user
     const result = await db.collection('users').insertOne({
       email,
       password: hashedPassword,
