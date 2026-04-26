80bedbfee48c9144ff6964cbbc470e422c97aae8
totomus83 <tom.dibellonio@gmail.com>
2026-04-26 18:57:23 +0200

    Task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,7 +3,8 @@ import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
-    const { email, password } = req.body || {};
+    const body = req.body || {};
+    const { email, password } = body;
 
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
